f42033b2b4b84a1aad9ae81a8625385c1cf7045d
momori85 <177555279+momori85@users.noreply.github.com>
2025-08-05 20:50:30 +0200

    appli

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DESKTOP_INSTALL_PATH="$HOME/.local/share/applications"
+ICON_INSTALL_PATH="$HOME/.local/share/icons/hicolor/256x256/apps"
+
+APP_NAME="WinDraw"
+EXEC_PATH="$(pwd)/$APP_NAME"
+ICON_PATH="icon.png"
+DESKTOP_FILE="$APP_NAME.desktop"
+
+mkdir -p "$ICON_INSTALL_PATH"
+mkdir -p "$DESKTOP_INSTALL_PATH"
+
+cp "$ICON_PATH" "$ICON_INSTALL_PATH/$APP_NAME.png"
+
+echo "[Desktop Entry]" > "$DESKTOP_FILE"
+echo "Version=1.0" >> "$DESKTOP_FILE"
+echo "Name=$APP_NAME" >> "$DESKTOP_FILE"
+echo "Comment=Application for drawing to the screen" >> "$DESKTOP_FILE"
+echo "Exec=$EXEC_PATH" >> "$DESKTOP_FILE"
+echo "Path=$(pwd)" >> "$DESKTOP_FILE"
+echo "Icon=$APP_NAME" >> "$DESKTOP_FILE"
+echo "Terminal=false" >> "$DESKTOP_FILE"
+echo "Type=Application" >> "$DESKTOP_FILE"
+echo "Categories=Utility;" >> "$DESKTOP_FILE"
+
+cp "$DESKTOP_FILE" "$DESKTOP_INSTALL_PATH/"
+chmod +x "$DESKTOP_INSTALL_PATH/$DESKTOP_FILE"
+
+xdg-icon-resource forceupdate --size 256
